d1e2726c2097f6a8e6a4600b170363f233109025
usama <usama@gmail.com>
2025-05-12 16:39:00 +0500

    Fixed dockerfile

--- Backend/middlewares/cors_middleware.py
+++ Backend/middlewares/cors_middleware.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
+        allow_origins=["https://doctorapp-production-c2f6.up.railway.app"],
         allow_origins=["http://localhost:5173"],
         allow_credentials=True,
         allow_methods=["*"],
